b8a2ee08ee6a53ee7164cbd0ba456db5cb29a4c9
DhiralKaniya <dhiralkaniya3146@gmail.com>
2017-06-04 17:13:56 +0530

    Manual Configuration file

---
+++ app/src/main/java/com/ramzproductview/Config/Config.java
@@ -0,0 +1,11 @@
+package com.ramzproductview.Config;
+
+/**
+ * Created by The_King on 6/2/2017.
+ */
+
+public class Config {
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final String IMAGE_DIRECTORY_NAME = "MyImages";
+    public static final int MEDIA_TYPE_IMAGE = 1;
+}
